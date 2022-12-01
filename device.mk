d82aee071eb083f2360da9b20e49c7bc25e4a9f6
Albert I <kras@raphielgang.org>
2022-12-01 17:10:15 +0100

    fog: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.

--- device.mk
+++ device.mk
@@ -258,6 +258,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-service \
