0699845ecb23194078d6a60c4cbd333e26993242
asyanx <wowsanja2@gmail.com>
2023-04-16 03:04:40 +0000

    fog: welcome stagOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fog.mk
+    $(LOCAL_DIR)/stag_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fog-user \
-    lineage_fog-userdebug \
-    lineage_fog-eng
+    stag_fog-user \
+    stag_fog-userdebug \
+    stag_fog-eng
